f67e63987ae1a69d6b7241597c224db0166be685
wendy-729 <38558926+wendy-729@users.noreply.github.com>
2022-01-13 21:09:59 +0800

    feat: 测试文件

---
+++ evaluate_abs_nopenalty_code1.m
@@ -0,0 +1,82 @@
+% 计算到截止日期,完全不考虑项目的完成概率
+function [expected_obj,time_pro,gap]=evaluate_abs_nopenalty_code1(AL,rep,implement,req,resNumber,nrpr,pred,nrsu,su,deadline,resNo,actNo,stochatic_d,code)
+gap = [];
+% 满足截止日期的次数
+p=0;
+% 总的目标函数值
+obj=0;
+% 随机工期数据
+index=1:rep;
+duration_data=stochatic_d(index,:);
+% 计算每个工期下的目标函数值
+for i=1:rep
+    d=duration_data(i,:);
+%  解码生成进度计划
+    [schedule, u_kt] = SSGS_PSGS2(AL,implement,req,resNumber,d,nrpr,pred,deadline,resNo,actNo,code);  
+    % 原来的解码
+%     [schedule, u_kt] = stochastic_SSGS(AL,implement,req,resNumber,d,nrpr,pred,deadline,resNo);
+    u_kt1=u_kt(:,1:schedule(actNo));
+    % 判断进度计划是否可行、资源可行
+    if scheduleFeasible(schedule,actNo,nrsu,su,implement,d)==1 && resourceFeasible(u_kt1,resNumber)==1 
+        if schedule(actNo)<=deadline
+            % 统计及时完成的次数
+            p=p+1;
+        else
+            gap_d = schedule(actNo)-deadline;
+            gap = [gap gap_d];
+        end
+        if schedule(actNo)<=deadline
+            for k=1:resNo
+%                 for t=2:deadline
+                for t=2:schedule(actNo)               
+                    if u_kt(k,t)-u_kt(k,t-1)<0
+                        temp = u_kt(k,t-1)-u_kt(k,t);
+                    else
+                        temp = u_kt(k,t)-u_kt(k,t-1);
+                    end
+                    obj=obj+temp;
+                end
+                obj = obj+u_kt(k,1);
+            end % 资源类型
+        else
+            % 超过了截止日期
+            for k=1:resNo
+    %             for t=2:deadline
+                for t=2:schedule(actNo)               
+                    if u_kt(k,t)-u_kt(k,t-1)<0
+                        temp = u_kt(k,t-1)-u_kt(k,t);
+                    else
+                        temp = u_kt(k,t)-u_kt(k,t-1);
+                    end
+                    obj=obj+temp;
+                end
+                obj = obj+u_kt(k,1);
+            end % 资源类型
+        end
+    end
+end %  情景数
+if rep==1
+    disp(schedule)
+    disp(u_kt)
+    obj_t = 0;
+    for k=1:resNo
+%         for t=2:deadline
+        for t=2:schedule(actNo)               
+            if u_kt(k,t)-u_kt(k,t-1)<0
+                temp = u_kt(k,t-1)-u_kt(k,t);
+            else
+                temp = u_kt(k,t)-u_kt(k,t-1);
+            end
+            if temp~=0
+                disp(temp)
+            end
+            obj_t=obj_t+temp;
+        end
+        obj_t = obj_t+u_kt(k,1);
+    end % 资源类型
+end
+
+expected_obj=obj/rep;
+time_pro=p/rep;
+
+                
